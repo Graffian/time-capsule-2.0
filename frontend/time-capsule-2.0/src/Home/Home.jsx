49f6d58e32308698de43b80ae93c3b74a3cba989
raymuos <soumyarjrout7@gmail.com>
2025-02-22 14:26:51 +0530

    added backround image and tagline

--- frontend/time-capsule-2.0/src/Home/Home.jsx
+++ frontend/time-capsule-2.0/src/Home/Home.jsx
@@ -18,16 +18,21 @@ function Home(){
           <p className="about-Text">ABOUT</p>
         </span>
       </nav>
-      <nav className="login-nav">
-        <label className="email-label" for="email">
-          EMAIL:
-        </label>
-        <input className="email-input" name="email" id="email" type="email" placeholder="someone@example.com" />
-        <label className="password-label" for="password">
-          PASSWORD:
-        </label>
-        <input className="password-input" name="password" id="password" type="password" placeholder="Password" />
-        <button className="signIn-btn">SIGN IN</button>
+      <nav className="page-content">
+        <p className="tagline">Gather your memories, dreams, and moments of joy, 
+          safely tucked away today, to be rediscovered and cherished by the
+          hearts of tomorrow ❤️</p>
+        <nav className="login-nav">
+          <label className="email-label" for="email">
+            EMAIL:
+          </label>
+          <input className="email-input" name="email" id="email" type="email" placeholder="someone@example.com" />
+          <label className="password-label" for="password">
+            PASSWORD:
+          </label>
+          <input className="password-input" name="password" id="password" type="password" placeholder="Password" />
+          <button className="signIn-btn">SIGN IN</button>
+        </nav>
       </nav>
     </>
     )
