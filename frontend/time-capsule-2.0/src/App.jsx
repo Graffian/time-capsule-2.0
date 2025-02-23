c637af58c725108a28e60dc679af8bc176ce7a8a
raymuos <soumyarjrout7@gmail.com>
2025-02-23 19:51:56 +0530

    first commit on second branch

--- frontend/time-capsule-2.0/src/App.jsx
+++ frontend/time-capsule-2.0/src/App.jsx
@@ -12,13 +12,20 @@ function App() {
   return (
     <>
       <nav className ="nav-bar">
-          <span className="nav-left">
-            <FontAwesomeIcon icon={faBars} />
-          </span>
+
         <span className="nav-center">
           TIME CAPSULE 2.0
         </span>
+        <span className="nav-left">
+           <FontAwesomeIcon icon={faBars} />
+        </span>
         <span className="nav-right">
+          <p onClick={()=>{navigate("/")}} className="home-Text">HOME</p>
+          <p className="capsules-Text">CAPSULES</p>
+          <p className="about-Text">ABOUT</p>
+        </span>
+        <span className="sidebar">
+          <p onClick={()=>{hideSidebar()}}><svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px" fill="#ffffff"><path d="m249-207-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z"/></svg></p>
           <p onClick={()=>{navigate("/")}} className="home-Text">HOME</p>
           <p className="capsules-Text">CAPSULES</p>
           <p className="about-Text">ABOUT</p>
