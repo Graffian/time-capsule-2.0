c9f2cc7721a9d67f3adbc0ef7a4c12e0700c3d56
raymuos <soumyarjrout7@gmail.com>
2025-02-23 09:47:27 +0530

    Complete Redesign of Explore page

--- frontend/time-capsule-2.0/src/Explore/Explore.jsx
+++ frontend/time-capsule-2.0/src/Explore/Explore.jsx
@@ -18,10 +18,14 @@ function Explore(){
           <input className="capsule-type-input" type="text" placeholder="SELECT TYPE....."/>
           
           <span className="date-input">
-            OPENING DATE:
-          <input className="capsule-type-input" type="date" placeholder="SELECT TYPE....."/>
-          CLOSING TIME:
-          <input className="capsule-type-input" type="time" placeholder="SELECT TYPE....."/>
+            <span className="open-date-input">
+              OPENING DATE:
+              <input className="capsule-type-input" type="date" placeholder="SELECT TYPE....."/>
+            </span>
+            <span className="close-time-input">
+              CLOSING TIME:
+              <input className="capsule-type-input" type="time" placeholder="SELECT TYPE....."/>
+            </span>
           </span>
           <span className="add-img-tab">
            <FontAwesomeIcon className="plusIcon" icon={faPlus} />
@@ -30,6 +34,13 @@ function Explore(){
             <button className="addBtn" onClick={handleAddBtn}>ADD CAPSULE</button>
         </div>
       </nav>
+
+      {/*------------CREATED CAPSULES SECTION------------------*/}
+      <p style={{fontSize:"3em", color:"white", marginTop:"50px",
+        backgroundImage:"linear-gradient(to right, #00a35f, #00B2FF)",
+        display:"flex", justifyContent:"center", alignContent:"Center"}}>
+          CREATED CAPSULES
+        </p>
       <nav className="created-capsule-nav">
         <div className="capsule">
           <span style={{"font-size":"1.4em"}} className="span-left">
@@ -44,7 +55,75 @@ function Explore(){
             <p className="opensInText">OPENS IN : 00:00:59</p>
           </span>
         </div>
+        <div className="capsule">
+          <span style={{"font-size":"1.4em"}} className="span-left">
+            INDEPENDENCE DAY
+          </span>
+          <span className="span-center">
+            `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus commodo magna a euismod. Vivamus sed nisi vitae dolor suscipit pellentesque in vel ligula. `
+          </span>
+          <span className="span-right">
+            <p className="creatorText">CREATOR:AYUSH</p>
+            <p className="createdOnTex">CREATED ON:12/02/2024</p>
+            <p className="opensInText">OPENS IN : 00:00:59</p>
+          </span>
+        </div>
+        <div className="capsule">
+          <span style={{"font-size":"1.4em"}} className="span-left">
+            INDEPENDENCE DAY
+          </span>
+          <span className="span-center">
+            `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus commodo magna a euismod. Vivamus sed nisi vitae dolor suscipit pellentesque in vel ligula. `
+          </span>
+          <span className="span-right">
+            <p className="creatorText">CREATOR:AYUSH</p>
+            <p className="createdOnTex">CREATED ON:12/02/2024</p>
+            <p className="opensInText">OPENS IN : 00:00:59</p>
+          </span>
+        </div>
+        <div className="capsule">
+          <span style={{"font-size":"1.4em"}} className="span-left">
+            INDEPENDENCE DAY
+          </span>
+          <span className="span-center">
+            `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus commodo magna a euismod. Vivamus sed nisi vitae dolor suscipit pellentesque in vel ligula. `
+          </span>
+          <span className="span-right">
+            <p className="creatorText">CREATOR:AYUSH</p>
+            <p className="createdOnTex">CREATED ON:12/02/2024</p>
+            <p className="opensInText">OPENS IN : 00:00:59</p>
+          </span>
+        </div>
+        <div className="capsule">
+          <span style={{"font-size":"1.4em"}} className="span-left">
+            INDEPENDENCE DAY
+          </span>
+          <span className="span-center">
+            `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus commodo magna a euismod. Vivamus sed nisi vitae dolor suscipit pellentesque in vel ligula. `
+          </span>
+          <span className="span-right">
+            <p className="creatorText">CREATOR:AYUSH</p>
+            <p className="createdOnTex">CREATED ON:12/02/2024</p>
+            <p className="opensInText">OPENS IN : 00:00:59</p>
+          </span>
+        </div>
+        <div className="capsule">
+          <span style={{"font-size":"1.4em"}} className="span-left">
+            INDEPENDENCE DAY
+          </span>
+          <span className="span-center">
+            `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus commodo magna a euismod. Vivamus sed nisi vitae dolor suscipit pellentesque in vel ligula. `
+          </span>
+          <span className="span-right">
+            <p className="creatorText">CREATOR:AYUSH</p>
+            <p className="createdOnTex">CREATED ON:12/02/2024</p>
+            <p className="opensInText">OPENS IN : 00:00:59</p>
+          </span>
+        </div>
       </nav>
+      <footer class="w-full bg-gray-800 text-white py-4">
+        <p class="text-sm">&copy; 2025 <span style={{fontFamily:"monospace", fontSize:"1.5em"}}> Team Graffian. </span> All rights reserved.</p>
+      </footer>
     </>
     )
 }
